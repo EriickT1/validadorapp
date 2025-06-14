f916758ca918625fd147f42c9f4439ed8531d667
EriickT1 <eriick1324@gmail.com>
2025-06-13 22:09:05 -0400

    Cambio de system.out por logger ( Recomendación sonarqube)

--- src/main/java/com/equipo/validador/App.java
+++ src/main/java/com/equipo/validador/App.java
@@ -1,13 +1,15 @@
 package com.equipo.validador;
-
+import java.util.logging.Logger;
 /**
  * Hello world!
  */
 public class App {
- public static void main(String[] args) {
- String usuario = System.getenv("APP_USER");
- if ("admin".equals(usuario)) {
- System.out.println("¡Bienvenido administrador!");
- }
- }
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        String usuario = System.getenv("APP_USER");
+        if ("admin".equals(usuario)) {
+            logger.info("¡Bienvenido administrador!");
+        }
+    }
 }
